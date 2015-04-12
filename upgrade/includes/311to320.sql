60bce33b96ab570c2e4b223a3a2325f1343761a4
DukeReborn <dukereborn@gmail.com>
2015-04-12 23:36:59 +0200

    Removed cmum dbversion from database

--- upgrade/includes/311to320.sql
+++ upgrade/includes/311to320.sql
@@ -1,2 +1,3 @@
 UPDATE `settings` SET `cmumversion`='3.2.0', `dbversion`='3.2.0' WHERE `id`=1;
 ALTER TABLE `groups` ADD `enabled` tinyint(1) NULL DEFAULT '1' AFTER `comment`;
+ALTER TABLE `settings` DROP `dbversion`;
