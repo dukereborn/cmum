0eb5d8f8ce6624e77515fe69b6734d69254cf511
DukeReborn <dukereborn@gmail.com>
2015-05-14 11:27:51 +0200

    Auto update check

--- upgrade/includes/311to320.sql
+++ upgrade/includes/311to320.sql
@@ -1,3 +1,5 @@
 UPDATE `settings` SET `cmumversion`='3.2.0', `dbversion`='3.2.0' WHERE `id`=1;
 ALTER TABLE `groups` ADD `enabled` tinyint(1) NULL DEFAULT '1' AFTER `comment`;
 ALTER TABLE `settings` DROP `dbversion`;
+ALTER TABLE `settings` ADD `autoupdcheck` TINYINT(1) NULL AFTER `soonexpusrorder`;
+UPDATE `settings` SET `autoupdcheck`='0' WHERE `id`=1;
