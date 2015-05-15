202ffba04e5a1b937a66d2f3c27cd265b58b02f4
DukeReborn <dukereborn@gmail.com>
2015-05-15 14:33:00 +0200

    Improved 404 error page on genxml

--- genxml.php
+++ genxml.php
@@ -16,9 +16,8 @@
 				print($xmldata);
 			exit;
 		} else {
-			header('HTTP/1.0 404 Not Found');
-				print("<h1>404 Not Found</h1>");
-				print("The page that you have requested could not be found.");
+			header("HTTP/1.0 404 Not Found");
+				print("<html><head></head><title>404 Not Found</title><body><h1>Not Found</h1><p>The requested URL was not found on this server.</p></body></html>");
 			exit();
 		}			
 ?>
