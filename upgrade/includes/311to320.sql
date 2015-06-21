2702076f962823b404dd098327ab450584c315fe
DukeReborn <dukereborn@gmail.com>
2015-06-21 21:29:50 +0200

    Support for IPv6 logging

--- upgrade/includes/311to320.sql
+++ upgrade/includes/311to320.sql
@@ -7,3 +7,5 @@ ALTER TABLE `log_activity` ADD `data` VARCHAR(50) NULL AFTER `userid`;
 TRUNCATE TABLE `log_activity`;
 ALTER TABLE `settings` ADD `usrorderby` VARCHAR(12) NULL AFTER `autoupdcheck`, ADD `usrorder` VARCHAR(4) NULL AFTER `usrorderby`;
 UPDATE `settings` SET `usrorderby`='user',`usrorder`='asc' WHERE `id`=1;
+ALTER TABLE `log_genxmlreq` CHANGE `ip` `ip` VARCHAR(45);
+ALTER TABLE `log_login` CHANGE `ip` `ip` VARCHAR(45);
