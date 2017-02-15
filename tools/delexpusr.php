dc447612e593d911d1acdbdf9e4636187420c713
dukereborn <dukereborn@gmail.com>
2017-02-15 20:53:13 +0100

    Fixed typo

--- tools/delexpusr.php
+++ tools/delexpusr.php
@@ -1,7 +1,7 @@
 <h4 class="header">Delete Expired Users</h4>
 	<form name="tools" action="tools.php?menu=1&tool=104" method="post" class="form">
 		<div class="control-group">
-			<label class="control-label" for="expdate">Expiredate (Users with expiredate olden than given date will be deleted)</label>
+			<label class="control-label" for="expdate">Expiredate (Users with expiredate older than given date will be deleted)</label>
 				<div class="controls">
 					<input type="text" name="expdate" id="expdate" data-date-format="yyyy-mm-dd" value="<?php if(isset($_POST["expdate"]) && $_POST["expdate"]<>"") { print($_POST["expdate"]); } else { print(date("Y-m-d")); } ?>">
 				</div>
