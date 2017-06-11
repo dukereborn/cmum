c1233637a0bf9909c3ba6fd9a1dbb4e7c978c162
dukereborn <dukereborn@gmail.com>
2017-06-11 21:24:20 +0200

    Branched version 3.3.3

--- includes/settings.php
+++ includes/settings.php
@@ -1,5 +1,5 @@
 <?php
 define("CMUM_TITLE","CSP MySQL User Manager");
-define("CMUM_VERSION","3.3.2");
-define("CMUM_BUILD","");
+define("CMUM_VERSION","3.3.3");
+define("CMUM_BUILD","trunk");
 define("CMUM_MOD","");
